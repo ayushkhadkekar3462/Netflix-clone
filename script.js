b5a38dc3468438a34e8f62bd7fa2fdca18b3aa4a
ayushkhadkekar3462 <ee220002022@iiti.ac.in>
2024-01-31 14:50:42 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,93 @@
+let created0,created1,created2,created3,created4,created5;
+document.getElementsByClassName("plus")[0].addEventListener("click",()=>{
+    created0.classList.remove("disappear")
+    created1.classList.add("disappear")
+created2.classList.add("disappear")
+created3.classList.add("disappear")
+created4.classList.add("disappear")
+created5.classList.add("disappear")
+    let created0=document.createElement("div");
+    created0.classList.add("class1");
+document.getElementsByClassName("item")[0].insertAdjacentElement("afterend",created0)
+
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+
+})
+
+document.getElementsByClassName("plus")[1].addEventListener("click",()=>{
+    created1.classList.remove("disappear")
+    created0.classList.add("disappear")
+created2.classList.add("disappear")
+created3.classList.add("disappear")
+created4.classList.add("disappear")
+created5.classList.add("disappear")
+    created1=document.createElement("div");
+    created1.classList.add("class1");
+document.getElementsByClassName("item")[1].insertAdjacentElement("afterend",created1)
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+
+})
+
+document.getElementsByClassName("plus")[2].addEventListener("click",()=>{
+    created2.classList.remove("disappear")
+    created1.classList.add("disappear")
+created0.classList.add("disappear")
+created3.classList.add("disappear")
+created4.classList.add("disappear")
+created5.classList.add("disappear")
+     created2=document.createElement("div");
+    created2.classList.add("class1");
+document.getElementsByClassName("item")[2].insertAdjacentElement("afterend",created2)
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+
+})
+
+document.getElementsByClassName("plus")[3].addEventListener("click",()=>{
+    created3.classList.remove("disappear")
+    created1.classList.add("disappear")
+created2.classList.add("disappear")
+created0.classList.add("disappear")
+created4.classList.add("disappear")
+created5.classList.add("disappear")
+    created3=document.createElement("div");
+    created3.classList.add("class1");
+document.getElementsByClassName("item")[3].insertAdjacentElement("afterend",created3)
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+})
+document.getElementsByClassName("plus")[4].addEventListener("click",()=>{
+    created4.classList.remove("disappear")
+    created1.classList.add("disappear")
+created2.classList.add("disappear")
+created3.classList.add("disappear")
+created0.classList.add("disappear")
+created5.classList.add("disappear")
+   created4=document.createElement("div");
+    created4.classList.add("class1");
+document.getElementsByClassName("item")[4].insertAdjacentElement("afterend",created4)
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+})
+document.getElementsByClassName("plus")[5].addEventListener("click",()=>{
+    created5.classList.remove("disappear")
+    created1.classList.add("disappear")
+    created2.classList.add("disappear")
+    created3.classList.add("disappear")
+    created4.classList.add("disappear")
+    created0.classList.add("disappear")
+
+     created5=document.createElement("div");
+    created5.classList.add("class1");
+document.getElementsByClassName("item")[5].insertAdjacentElement("afterend",created5)
+document.querySelector("box").classList.add("class2")
+document.querySelector("dabba").classList.add("class2")
+
+})
